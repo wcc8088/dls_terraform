ff44be5118bb80ce315080e1aad22ebe7f79babf
wcc8088 <wcc8088@naver.com>
2023-10-24 14:17:40 +0900

    03

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.0"
 
-  name                 = "SHBW-AN2-DLS-PRD-VPC-DLS-NW-01"
+  name                 = "SHL-AN2-DOS-DEV-VPC-DOS-NW-01"
   cidr                 = "10.0.0.0/16"
   azs                  = ["${var.region}a", "${var.region}b", "${var.region}c"]
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
